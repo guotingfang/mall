2369a2cd8fd441f5271afe68ce41d87dcd0258cc
guotingfang <guo_ting_fang@163.com>
2020-04-27 22:31:22 +0800

    项目第一次提交

---
+++ mall/src/main/java/com/imooc/mall/dao/CategoryMapper.java
@@ -0,0 +1,19 @@
+package com.imooc.mall.dao;
+
+import com.imooc.mall.pojo.Category;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+
+/**
+ * Created by 廖师兄
+ * 2035-01-18 18:25
+ */
+//@Mapper
+public interface CategoryMapper {
+
+	@Select("select * from mall_category where id = #{id}")
+	Category findById(@Param("id") Integer id);
+
+	Category queryById(@Param("id") Integer id);
+}
